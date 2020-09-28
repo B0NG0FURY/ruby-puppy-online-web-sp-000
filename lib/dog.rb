a92a691be710d6feb0672f690d557d5483923c8f
John Troutman <jtroutmanman@gmail.com>
2020-09-28 17:37:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
 # Add your code here
+class Dog
+  @@all = []
+  attr_reader :name
+  
+  def initiate(name)
+    @name = name
+    save
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+end
